ae374f8d2054b3726fb4a100bedc85f7e3f9c6aa
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-18 10:59:12 +0500

    Task019 Palindrome

---
+++ Task019/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число от 10000 до 99999: ");
+int number = int.Parse(Console.ReadLine()!);
+int number1 = (number / 10000);
+int number2 = (number / 1000) % 10;
+int number3 = (number / 100) % 10;
+int number4 = (number / 10) % 10;
+int number5 = (number % 10);
+
+if (number1 == number5 && number2 == number4)
+{
+    Console.WriteLine($"Число: {number}, является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число: {number}, не является палиндромом");
+}
