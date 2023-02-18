08534d667f3b38fb24e9e4eebe681bca9c2da2c4
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-18 18:21:52 +0500

    Task023 table of cubes of number N

---
+++ Task023/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int i = 1;
+int m = N;
+
+if (N < 0)
+{
+    i = N;
+    m = -N;
+    N = -1;
+}
+
+int[] result = new int [m];
+
+for(int j = 0; i <= N; i++)
+{
+    result[j] = i * i * i;
+    j++;
+}
+for(int j = 0; j < m; j++)
+{
+    Console.WriteLine($"{result[j]}");
+}
