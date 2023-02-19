612da067168f9b8b5a16badd050df165d3e95c41
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-19 22:47:01 +0500

    Task019 Palindrome - converting a number to an array

--- Task019/Program.cs
+++ Task019/Program.cs
@@ -1,16 +1,35 @@
-﻿Console.Write("Введите число от 10000 до 99999: ");
+﻿Console.Write("Определение палиндрома, ведите число: ");
 int number = int.Parse(Console.ReadLine()!);
-int number1 = (number / 10000);
-int number2 = (number / 1000) % 10;
-int number3 = (number / 100) % 10;
-int number4 = (number / 10) % 10;
-int number5 = (number % 10);
+int N = number;
+int i = 0;
+int count = 0;
 
-if (number1 == number5 && number2 == number4)
+int[] result = new int [N];
+
+for (int j = 0; N >= 1; i++)
 {
-    Console.WriteLine($"Число: {number}, является палиндромом");
+    result[j] = N % 10;
+    N = N / 10;
+    j++;
+    count++;
 }
-else
+
+for (int j = 0; j < count; j++)
 {
-    Console.WriteLine($"Число: {number}, не является палиндромом");
+    Console.Write($"{result[j]}");
 }
+
+//int number1 = (number / 10000);
+//int number2 = (number / 1000) % 10;
+//int number3 = (number / 100) % 10;
+//int number4 = (number / 10) % 10;
+//int number5 = (number % 10);
+
+//if (number1 == number5 && number2 == number4)
+//{
+//    Console.WriteLine($"Число: {number}, является палиндромом");
+//}
+//else
+//{
+//    Console.WriteLine($"Число: {number}, не является палиндромом");
+//}
